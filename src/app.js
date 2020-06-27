8ea2c2edb11b72c3fd02086c7d70c05c6c810eb9
rafael <djalmasantos839@gmail.com>
2020-06-26 22:25:07 -0300

    salvando o desafio GOstack

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+ const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,70 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { 
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+   const {id } = request.params.id;
+   const { url, title,  techs } = request.body;
+   const repository = repositories.find(repo => repo.id === id );   
+   
+
+   if (!repository) 
+   {   return response.status(400).send().json({ error: 'Repositório Inexistente'});      }
+   
+   
+   repository.url = url;
+   repository.title = title;
+   repository.techs = techs;
+
+   return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id } = request.params.id;  
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id );  
+  if (repositoryIndex < 0) 
+  {   
+    return response.status(400).send().json({ error: 'Repositório Inexistente'});
+  }   
+  
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  /// encontrando o repositorio dentro do array pelo id no params
+  const repository = repositories.find(repo => repo.id === id );
+  
+  if (!repository) 
+   {   return response.status(400).send();      }
+
+  repository.likes++;
+  
+  return response.json(repository);
+
+
 });
 
 module.exports = app;
